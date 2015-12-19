6b2bfc2a9b3c4dc962d185900c545c0bc5751fe0
Kevin Abiera <kevin@abiera.com>
2015-12-19 10:11:52 +0800

    Use UUIDModel for User

--- rcj-leaderboards/users/models.py
+++ rcj-leaderboards/users/models.py
@@ -7,9 +7,11 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+from ..core.models import UUIDModel
+
 
 @python_2_unicode_compatible
-class User(AbstractUser):
+class User(UUIDModel, AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
